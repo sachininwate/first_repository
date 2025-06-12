b040b5c33f88202503cb6e3e563e040cca2904c9
sachininwate <163038552+sachininwate@users.noreply.github.com>
2025-06-12 23:30:38 +0530

    Create firstpython.py
    first python program

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+# This is a program to check whether entered number is even or not
+n = int(input("Enter number"))
+if n % 2 == 0:
+  print(n , " is even number")
+else:
+  print(n, " is odd number")
